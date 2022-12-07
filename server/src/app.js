e51c9472aa98589aa5b7fb6392ffe46bbdd8b674
Shawn Smith <sds.smith24@gmail.com>
2022-12-07 07:51:36 -0500

    install and implement helmet middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const api = require('./routes/api')
 
 const app = express();
 
+app.use(helmet());
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
