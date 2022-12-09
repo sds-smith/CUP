b001a78ee4a5b041a194b8d6e0f1a0365f9eea47
Shawn Smith <sds.smith24@gmail.com>
2022-12-09 11:22:07 -0500

    clean up sign in component

--- client/src/components/forms/sign-in/sign-in.component.jsx
+++ client/src/components/forms/sign-in/sign-in.component.jsx
@@ -1,7 +1,6 @@
 
 const SignIn = () => {
     return (
-        // <button className='SignInButton' onClick={signIn}>{buttonText}</button>
         <a href='https://localhost:8000/v1/auth/google'>Log In with Google</a>
     )
 }
